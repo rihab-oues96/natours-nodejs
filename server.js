0cb8b1037435eeccfe216e8c692742a0a92e6423
Rihab.oueslati <rihaboueslati@takiacademyteam.com>
2023-02-15 18:03:38 +0100

    add  mongoos

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
-// console.log(process.env);
+// connect app width database throw mongoose
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(con => console.log('DB connections successful'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
